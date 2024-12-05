bfd6f6537992ad50b42d852ebfb48821363f2dac
Pedro Afonso <sirafonso005896@outlook.com>
2024-12-05 16:05:20 -0300

    :recyle: fix user controller types

--- src/main/java/com/sirafonso/e_library_users/adapters/in/web/controllers/UsersController.java
+++ src/main/java/com/sirafonso/e_library_users/adapters/in/web/controllers/UsersController.java
@@ -1,7 +1,14 @@
 package com.sirafonso.e_library_users.adapters.in.web.controllers;
 
+import com.sirafonso.e_library_users.adapters.in.web.controllers.dtos.SaveUserRequest;
+import com.sirafonso.e_library_users.core.domain.models.UserModelIn;
+import com.sirafonso.e_library_users.core.domain.models.UserModelOut;
 import com.sirafonso.e_library_users.core.ports.in.*;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("users")
@@ -28,28 +35,28 @@ public class UsersController {
     }
 
     @PostMapping
-    String createNewUser(@RequestBody String username) {
-        return this.saveUser.execute(username);
+    UserModelOut createNewUser(@RequestBody @Valid SaveUserRequest newUser) {
+        return this.saveUser.execute(newUser.toUserModelIn());
     }
 
     @GetMapping
-    String getAllUsers() {
+    List<UserModelOut> getAllUsers() {
         return this.getAllUsers.execute();
     }
 
     @GetMapping("/{userId}")
-    String getSingleUser(@PathVariable String userId) {
+    UserModelOut getSingleUser(@PathVariable UUID userId) {
         return this.getSingleUser.execute(userId);
     }
 
     @PatchMapping("/{userId}")
-    String updateUserUsername(@PathVariable String userId, @RequestBody String username) {
+    UserModelOut updateUserUsername(@PathVariable UUID userId, @RequestBody String username) {
         return this.updateUserUsername.execute(userId, username);
     }
 
     @DeleteMapping("/{userId}")
-    String deleteUser(@PathVariable String userId) {
-        return this.deleteUser.execute(userId);
+    void deleteUser(@PathVariable UUID userId) {
+        this.deleteUser.execute(userId);
     }
 
 }
